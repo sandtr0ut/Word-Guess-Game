1fb3a479a68e1e4bf300ee16c0831f9b80c9e8f9
sandtr0ut <47204349+sandtr0ut@users.noreply.github.com>
2019-03-06 19:29:14 -0700

    updates in vain

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+window.onload = function () {
 // Word Guess Game
 
 // Globals
@@ -6,7 +7,7 @@
 var words = ["appetizer", "apple pie", "bake", "baked Alaska", "baking soda", "banana split", "basil", "beef", "BLT", "Bundt pan", "cheese", "corn", "crepe", "doughnuts", "egg nog", "fork", "freeze", "fruit salad", "grape", "KFC", "lemon zester", "mac and cheese", "McDonalds", "melt", "muffin tin", "pear", "poach", "potato chips", "produce", "rolling pin"];
 
 //create array for all letters in the alphabet
-var allowedInput = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// var allowedInput = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // Create variables to hold references to html insertion points
 var titleBlock = document.getElementById("title-block");
@@ -17,61 +18,181 @@ var guessResultText = document.getElementById("guess-result");
 // var playAgain = document.getElementById("play-again");
 var guessWordText = document.getElementById("guess-word");
 var alreadyGuessedText = document.getElementById("guessed-already");
-var guessesRemainingCount = document.getElementById("guesses-remainingLetters");
+var guessesRemainingCount = document.getElementById("guesses-remaining");
+
+//other global variables
+var wordLetters;
+var matchedLetters;
+var alreadyGuessed;
+var guessesRemaining = 0;
+var totalGuesses = 0;
+var playerWins = 0;
+var letterGuessed = null;
+var word = null;
 
 
-//computer chooses a word at random from the word list array [word = word]
-var word = words[Math.floor(Math.random() * words.length)];
+//game setup on page load
+function setupGame () {
+  //computer chooses a word at random from the word list array [word = word]
+  var word = words[Math.floor(Math.random() * words.length)];
+  wordLetters = word.split("");
+  
+  displayWord();
+  
+  setTotalGuesses();
 
-//set up array to show how many letters there are using underscores in place of the letters
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
+
+//runs when player selects a letter
+
+function statusUpdate(letter) {
+  if (guessesRemaining === 0) {
+    restartGame();
+    
+  } else {
+    //identify incorrect guesses and trigger function to display
+    updateGuesses(letter);
+    
+    //identify correct guesses and trigger function to display
+    updateMatchedLetters(letter);
+    
+    //show on-screen results of player guess
+    displayWord();
+    
+    //restart game if player wins
+    if (updatePlayerWins() === true) {
+      restartGame();      
+    }
+  }
 }
-//create variable to hold number of letters left to be guessed
-var remainingLetters = word.length;
+//if player guesses wrong
+function updateGuesses(letter) {
+  if ((alreadyGuessed.indexOf(letter) === -1) &&
+     (wordLetters.indexOf(letter) === -1)) {
+    
+      //add to alreadyGuessed array
+      alreadyGuessed.push(letter);
+      
+      //decrease guessesRemaining by 1
+      guessesRemaining--;
+      
+      //update page
+      guessesRemainingCount.innerHTML = guessesRemaining;
+      alreadyGuessedText.innerHTML = alreadyGuessed.join(", ");
+    
+  }
+  }
+  
+  //function to set the total number of guesses the player gets
+  function setTotalGuesses() {
+    totalGuesses = wordLetters.length + 5;
+    guessesRemaining = totalGuesses;
+    
+    //update page with guesses remaining
+    guessesRemainingCount.innerHTML = guessesRemaining;
+  }
+  
+  // if the player guesses correctly
+  function updateMatchedLetters(letter) {
+    //run through wordLetters array
+    for (var i = 0; i < wordLetters.length; i++) {
+      // If the guessed letter is in the solution, and it has yet to be guessed
+      if ((letter === wordLetters[i]) && (matchedLetters.indexOf(letter) === -1)) {
+        // Push letter into matchedLetters array.
+        matchedLetters.push(letter);
+      }
+    }    
+  }
 
+//set up display of current word using underscores in place of the letters
+function displayWord() {
+  //start with empty string
+  var wordDisplay = "";
+  
+  //run through wordLetters array
+  for (var i = 0; i < wordLetters.length; i++) {
+    //if the guessed letter is a match, display it
+    if (matchedLetters.indexOf(wordLetters[i]) !== -1) {
+      wordDisplay += wordLetters[i];
+      }
+      //if not, display an "_"
+      else {
+        wordDisplay += "&nbsp;_&nbsp;";
+      }    
+    }
+    //update page with new string
+    guessWordText.innerHTML = wordDisplay;  
+}
 
-// ******Here Begins Ye Old Main Game******
+// Function that resets the game
+function restartGame() {
+  alreadyGuessedText.innerHTML = "";
+  word = null;
+  wordLetters = [];
+  matchedLetters = [];
+  alreadyGuessed = [];
+  guessesRemaining = 0;
+  totalGuesses = 0;
+  letterGuessed = null;
+  setupGame();
+  displayWord();  
+}
 
-//Player responds to initial welcome message and presses any key
-document.onkeyup = function (event) {
-  changeMsgText.innerHTML = "SO IT BEGINS...challenge word selected";
-  addMsgText.innerHTML = "Select a letter!";
+//check if user has won
+function updatePlayerWins() {
+  var win;
+  if (matchedLetters.length === 0) {
+    win = false;
+  }
+  else {
+    win = true;
+  }
+  // If a letter appears in the wordLetters array, but not in the matchedLetters array, set win to false.
+  for (var i = 0; i < wordLetters.length; i++) {
+    if (matchedLetters.indexOf(wordLetters[i]) === -1) {
+      win = false;
+    }
+  }
+  
+  // If win is true...
+  if (win) {
+
+    // Increment wins.
+    playerWins = playerWins + 1;
+
+    // Update wins on the page.
+    playerWinsCount.innerHTML = playerWins;
+  
+    return true;
 }
   
-  //while there are still letters left to guess
-  while (remainingLetters > 0){
-    //show progress
-    guessWordText.innerHTML(answerArray.join(" "));
+return false;
+}
+}
+
+    //clear the game info when page is loaded || reloaded
+    setupGame();
     
-    //convert the guessed letter to lower case and
-    //store it in a var ***(do we need to run onkeyup again here? careful)***
-    //***mind the scope***
-    document.onkeyup = function(event) {
-      
-    //determine which key is pressed
-    var guessLetter =  event.key;
-    var guess = guessLetter.toLowerCase();
-    
-    //if the letter that is guessed is in allowedInput[]
-    var allowed = allowedInput.includes(guessLetter);
-    if (allowed == false) {
-      alert("letters only please!");
-    }else{
-      //update the game state
-      for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-              //update the answer array with the letter they guessed
-              answerArray[j] = guess;
-              //subtract one from remaining letters
-              remainingLetters--;
-            }
-          }
-        }
+    //listen for keyup, check if it is a letter, convert to lower case, and store it
+    document.onkeyup = function (event) {
+      letterGuessed = event.key.toLowerCase();
+      if (allowedInput.includes(letterGuessed)) {
+        
+        //update the welcome message and instructions
+        changeMsgText.innerHTML = "SO IT BEGINS...challenge word selected";
+        addMsgText.innerHTML = "Select a letter!";
+        
+        
+        var test = "you pressed a letter";
+        console.log(test);
+      }else {
+        var test = "I don't know what the fuck you pressed";
+        console.log(test);
+        
+        alert("Select letters only please!")
       }
-    }
-    if (remainingLetters = 0) {
-      guessWordText.innerHTML(answerArray.join(" "));
-      
+        console.log(letterGuessed);
+        
+      }
+    
+    
     }
